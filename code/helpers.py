9bd8c67044c2f7b8a6a29c7b95a3492e6f6ffdb4
Seb <teoutufekoi@gmail.com>
2020-01-19 22:42:49 +0100

    Add helper.py

---
+++ code/helpers.py
@@ -0,0 +1,24 @@
+import click
+import binascii
+import os
+from cookbook import *
+from database import *
+
+
+def generate_gid():
+    nbytes = 4
+    random_bytes = os.urandom(nbytes)
+    return binascii.hexlify(random_bytes).decode('ascii')
+
+
+def list_recipes(recipes):
+    print('\n'.join(str(i.name) for i in recipes))
+
+
+def get_unit(key, unit_list):
+    unit = next((item for item in all_units if item.gid == key), None)
+    if unit is None:
+        return "n/a"
+    else:
+        return unit.short
+
